f54ccaafb8a18e5b598d4c9cb104c201c5e2d786
David Stetz <David.Stetz@e-hps.com>
2019-07-08 17:25:46 -0700

    Added Telerik renderers

---
+++ TipCalc.Forms.Web/Linker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WasabiUI.Telerik.Renderers;
+
+namespace TipCalc.Forms.Web
+{
+    public class Linker
+    {
+        public void LinkWasabiTelerik()
+        {
+            var type = typeof(RadEntryRenderer);
+        }
+    }
+}
